a169922ac4c1c98600990032c568f6b05853b28a
arjunraghav <arjunraghav007@gmail.com>
2022-01-31 12:07:10 +0530

    fixed file renaming

--- app/models.py
+++ app/models.py
@@ -5,9 +5,7 @@ from .managers import CustomUserManager
 # Create your models here.
 
 
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    # return 'user_{0}/{1}'.format(instance.user.id, filename)
+def filename_naming_handler(instance, filename):
     extension = filename.split('.')[-1]
     return f"upload-ID-{instance.user_id}-{instance.title}-{instance.timestamp}.{extension}"
 
@@ -30,7 +28,7 @@ class UserFile(models.Model):
     user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
-    file = models.FileField(upload_to=user_directory_path)
+    file = models.FileField(upload_to=filename_naming_handler)
 
     def __str__(self):
         return f"upload-ID-{self.user_id}-{self.title}-{self.timestamp}"
